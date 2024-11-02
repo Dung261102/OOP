bc2c8bd28b0d3f1a1ccdb64e16bf31fd8be63443
Dung261102 <20521211@gm.uit.edu.vn>
2024-11-03 03:53:59 +0700

    quản lý hình ảnh và vị trí của các block

---
+++ Pikachu/Assets/_Data/Grid/BlockData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockData : BlockAbstract
+{
+    [Header("BlockData")]
+    // Thu?c tính l?u tr? hình ?nh (sprite) ??i di?n cho Block trong game.
+    public Sprite sprite;
+  
+    public Node node;
+
+    //Hàm setnot
+    public virtual void SetNode(Node node)
+    {
+        this.node = node;
+    }
+
+    //Hàm lấy hình ảnh
+    public virtual void SetSprite(Sprite sprite)
+    {
+        this.sprite = sprite;
+        this.ctrl.sprite.sprite = sprite;
+    }
+}
