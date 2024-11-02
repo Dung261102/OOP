9c3f7048f8521bea9c80572a6d64bed013e4d06b
Dung261102 <20521211@gm.uit.edu.vn>
2024-11-03 00:22:16 +0700

    add GridSystem

--- Pikachu/Assets/_Data/Grid/GridSystem.cs
+++ Pikachu/Assets/_Data/Grid/GridSystem.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
-public class GridSystem : SaiMonoBehaviour
+public class GridSystem : GridAbstract
 {
-
+    [Header("Grid System")]
     //Kích thước của lưới
     public int width = 18;
     public int height = 11;
+    public float offsetX = 0.2f; //Khoảng cách giữa các node
     public List<Node> nodes;
+
+    protected override void Start()
+    {
+        this.SpawnBlocks();
+    }
 
     protected override void LoadComponents() //reset hiện được node lên
     {
@@ -21,6 +27,7 @@ public class GridSystem : SaiMonoBehaviour
     //Hàm hiện node lên
     protected virtual void InitGridSystem()
     {
+
         if (this.nodes.Count > 0) return;
 
        
@@ -32,7 +39,8 @@ public class GridSystem : SaiMonoBehaviour
                 {
                     x = x,
                     y = y,
-       
+                    posX = x - (this.offsetX * x),
+
                 };
 
                 this.nodes.Add(node);
@@ -44,17 +52,22 @@ public class GridSystem : SaiMonoBehaviour
     //Hàm SpawnBlocks()
     protected virtual void SpawnBlocks()
     {
+        
         Vector3 pos = Vector3.zero;
         
         foreach (Node node in this.nodes)
-        {  
-                
-                pos.x = node.x;
-                pos.y = node.y;
-       
+        {
+            if (node.x == 0) continue; //Bỏ đi hàng đầu tiên
+            if (node.y == 0) continue; 
+            if (node.x == this.width-1) continue;
+            if (node.y == this.height-1) continue; //Bỏ đi hàng đaauf tiên
 
-                Transform block = BlockSpawner.Instance.Spawn(BlockSpawner.BLOCK, pos, Quaternion.identity);
-                block.gameObject.SetActive(true);
+            pos.x = node.posX; //khoảng cách giữa 2 node theo chiều ngang
+            pos.y = node.y;
+
+
+            Transform block = this.ctrl.blockSpawner.Spawn(BlockSpawner.BLOCK, pos, Quaternion.identity);
+            block.gameObject.SetActive(true);
 
         }
         
