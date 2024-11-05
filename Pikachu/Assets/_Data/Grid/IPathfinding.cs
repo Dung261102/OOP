412db05bab3a8723220697bcb24e16fde8ba7f41
Dung261102 <20521211@gm.uit.edu.vn>
2024-11-05 13:53:25 +0700

    Thử giải thuật tìm đường đầu tiên

---
+++ Pikachu/Assets/_Data/Grid/IPathfinding.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IPathfinding
+{
+    public abstract void FindPath (BlockCtrl startBlock, BlockCtrl targetBlock);
+
+
+
+}
