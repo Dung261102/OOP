f20aa9efdd8d020f9e13f5166cd9a1962e61871b
Dung261102 <20521211@gm.uit.edu.vn>
2024-11-05 13:59:13 +0700

    Thêm textmeshpro cho mỗi block để phân biệt dễ dàng hơn

--- Pikachu/Assets/_Data/Grid/BlockData.cs
+++ Pikachu/Assets/_Data/Grid/BlockData.cs
@@ -1,25 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BlockData : BlockAbstract
 {
     [Header("BlockData")]
-    // Thu?c tính l?u tr? hình ?nh (sprite) ??i di?n cho Block trong game.
-    public Sprite sprite;
-  
+    public TextMeshPro text;
+
+    // Thuộc tính lưu thông tin của node mà block này đang thuộc về
     public Node node;
 
-    //Hàm setnot
+    // Thuộc tính lưu trữ hình ảnh (sprite) đại diện cho Block trong game
+    public Sprite sprite;
+    
+    protected override void LoadComponents()
+    {
+        base.LoadComponents();
+
+        this.LoadTextMeshPro();
+    }
+
+    protected virtual void LoadTextMeshPro()
+    {
+        if (this.text != null) return;
+        this.text = transform.GetComponentInChildren<TextMeshPro>();
+        Debug.LogWarning(transform.name + " LoadTextMeshPro", gameObject);
+    }
+
+
+
+    // Hàm thiết lập node cho block
     public virtual void SetNode(Node node)
     {
         this.node = node;
+        //mã số 
+        this.text.text = this.node.x.ToString() + "\n" +  this.node.y.ToString(); 
     }
 
     //Hàm lấy hình ảnh
     public virtual void SetSprite(Sprite sprite)
     {
-        this.sprite = sprite;
+        //this.sprite = sprite;
         this.ctrl.sprite.sprite = sprite;
     }
 }
