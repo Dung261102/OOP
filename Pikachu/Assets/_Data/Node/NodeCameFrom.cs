c0f052b5415501fca2b4a8a4a9e142fc4b0fbc66
Dung261102 <20521211@gm.uit.edu.vn>
2024-11-07 02:44:27 +0700

    show node

---
+++ Pikachu/Assets/_Data/Node/NodeCameFrom.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[Serializable]
+public class NodeCameFrom
+{
+    public string nodeId;
+    public NodeDirections direction = NodeDirections.noDirection;
+
+
+    public Node fromNode;
+
+    public Node toNode;
+
+    public NodeCameFrom(Node fromNode, Node toNode)
+    {
+        this.nodeId = fromNode.x + "x" + fromNode.y;
+
+        this.toNode = fromNode;
+        this.fromNode = toNode;
+        this.direction = this.GetDirection( fromNode, toNode);
+
+    }
+
+    protected virtual NodeDirections GetDirection(Node fromNode, Node toNode)
+    {
+        if (fromNode.x == toNode.x && fromNode.y < toNode.y) return NodeDirections.up;
+        if (fromNode.x == toNode.x && fromNode.y > toNode.y) return NodeDirections.down;
+
+        if (fromNode.x < toNode.x && fromNode.y == toNode.y) return NodeDirections.right;
+        if (fromNode.x > toNode.x && fromNode.y == toNode.y) return NodeDirections.left;
+
+        return NodeDirections.noDirection;
+
+    }
+}
