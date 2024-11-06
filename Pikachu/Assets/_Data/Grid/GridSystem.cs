d920dbf74af5fe8e45a6da859a8c89b58d7ddc1c
Dung261102 <20521211@gm.uit.edu.vn>
2024-11-06 13:50:24 +0700

    Thêm spawnHolder

--- Pikachu/Assets/_Data/Grid/GridSystem.cs
+++ Pikachu/Assets/_Data/Grid/GridSystem.cs
@@ -13,12 +13,30 @@ public class GridSystem : GridAbstract
     public float offsetX = 0.2f; //Khoảng cách giữa các node
     public BlocksProfile blocksProfile;
     public List<Node> nodes;
+   
     public List<BlockCtrl> blocks;
     public List<int> nodeIds;
 
 
+   
+    
+    protected override void LoadComponents() //reset hiện được node lên
+    {
+        base.LoadComponents();
+        this.InitGridSystem();
+        this.LoadBlockProflie();
+    }
+
+    protected virtual void LoadBlockProflie()
+    {
+        if (this.blocksProfile != null) return;
+        this.blocksProfile = Resources.Load<BlocksProfile>("Pikachu");
+        Debug.LogWarning(transform.name + " LoadBlockProflie", gameObject);
+    }
+
     protected override void Start()
     {
+        this.SpawnHolders();
         this.SpawnBlocks();
         this.FindNodesNeighbors();
         this.FindBlocksNeighbors();
@@ -61,66 +79,68 @@ public class GridSystem : GridAbstract
         }
     }
 
-    protected override void LoadComponents() //reset hiện được node lên
-    {
-        base.LoadComponents();
-        this.InitGridSystem();
-        this.LoadBlockProflie();
-    }
-
-    protected virtual void LoadBlockProflie()
-    {
-        if (this.blocksProfile != null) return;
-        this.blocksProfile = Resources.Load<BlocksProfile>("Pokemons");
-        Debug.LogWarning(transform.name + " LoadBlockProflie", gameObject);
-    }
 
     //Hàm hiện node lên
     protected virtual void InitGridSystem()
     {
+        if (this.nodes == null) this.nodes = new List<Node>();
+        if (this.nodeIds == null) this.nodeIds = new List<int>();
 
-        if (this.nodes.Count > 0) return;
+        if (this.nodes.Count > 0) return; // Đảm bảo không khởi tạo lại nếu danh sách node đã có
 
         int nodeId = 0;
         for (int x = 0; x < this.width; x++)
         {
             for (int y = 0; y < this.height; y++)
             {
-                Node node = new()
+                Node node = new Node
                 {
                     x = x,
                     y = y,
                     posX = x - (this.offsetX * x),
                     nodeId = nodeId,
-
                 };
 
                 this.nodes.Add(node);
-                this.nodeIds.Add(nodeId);
+                this.nodeIds.Add(nodeId); // Thêm nodeId vào danh sách nodeIds
                 nodeId++;
             }
         }
+
+        Debug.Log("Total nodes created: " + this.nodes.Count); // Thêm thông báo debug
+        Debug.Log("Total node IDs available: " + this.nodeIds.Count); // Thêm thông báo debug
     }
 
+
     //done
-    protected virtual void SpawnNodeObj()
+    protected virtual void SpawnHolders()
     {
+      
+
         Vector3 pos = Vector3.zero;
         foreach (Node node in this.nodes)
         {
-            if (node.x == 0) continue;
-            if (node.y == 0) continue;
-            if (node.x == this.width - 1) continue;
-            if (node.y == this.height - 1) continue;
+
 
             pos.x = node.posX;
             pos.y = node.y;
 
-            Transform obj = this.ctrl.blockSpawner.Spawn(BlockSpawner.BLOCK, pos, Quaternion.identity);
-            BlockCtrl blockCtrl = obj.GetComponent<BlockCtrl>();
+            Transform blockObj = this.ctrl.blockSpawner.Spawn(BlockSpawner.HOLDER, pos, Quaternion.identity);
 
-            
-            obj.gameObject.SetActive(true);
+           
+
+            // Lấy component NodeTransform từ blockObj
+            NodeTransform blockHolder = blockObj.GetComponent<NodeTransform>();
+
+          
+
+            node.nodeTranform = blockHolder;
+            blockObj.name = "Holder_" + node.x.ToString() + "_" + node.y.ToString();
+            blockHolder.gameObject.SetActive(true);
+
+
+            blockObj.gameObject.SetActive(true);
+            node.occupied = true;
         }
     }
 
@@ -137,6 +157,16 @@ public class GridSystem : GridAbstract
                 pos.x = node.posX;
                 pos.y = node.y;
 
+                //lấy toạ độ của block
+                Transform blockObj = this.ctrl.blockSpawner.Spawn(BlockSpawner.HOLDER, pos, Quaternion.identity);
+                NodeTransform blockHolder = blockObj.GetComponent<NodeTransform>();
+                node.nodeTranform = blockHolder;
+                blockObj.name = "Holder_" + node.x.ToString() + "_" + node.y.ToString();
+
+
+                blockHolder.gameObject.SetActive(true);
+
+
                 Transform block = this.ctrl.blockSpawner.Spawn(BlockSpawner.BLOCK, pos, Quaternion.identity);
 
                 BlockCtrl blockCtrl = block.GetComponent<BlockCtrl>();
@@ -144,7 +174,7 @@ public class GridSystem : GridAbstract
                 //Lấy hình ảnh
                 blockCtrl.blockData.SetSprite(sprite);
 
-                this.LinkNodeBlock(node, blockCtrl); //kết nối 2 node lại với nhau
+                this.LinkNodeBlock(blockCtrl, node); //kết nối 2 node lại với nhau
                 block.name = "Block_" + node.x.ToString() + "_" + node.y.ToString();
 
 
@@ -152,12 +182,19 @@ public class GridSystem : GridAbstract
             }
         }
     }
-    
+
     //done
-    
+
     protected virtual Node GetRandomNode()
     {
-       
+        //chatgpt
+        // Kiểm tra danh sách nodeIds trước khi cố gắng lấy ngẫu nhiên
+        //if (this.nodeIds.Count == 0)
+        //{
+        //    Debug.LogError("No available nodes to choose from.");
+        //    return null;
+        //}
+        //end
 
         Node node;
         int randId;
@@ -170,7 +207,7 @@ public class GridSystem : GridAbstract
             randId = Random.Range(0, this.nodeIds.Count);
             node = this.nodes[this.nodeIds[randId]];
 
-            
+
             this.nodeIds.RemoveAt(randId);
 
             if (node.x == 0) continue;
@@ -186,10 +223,14 @@ public class GridSystem : GridAbstract
     }
 
 
+    
+
+
+
     //done
-    protected virtual void LinkNodeBlock(Node node, BlockCtrl blockCtrl)
+    protected virtual void LinkNodeBlock( BlockCtrl blockCtrl, Node node)
     {
-        blockCtrl.blockData.SetNode(node);
+        blockCtrl.blockData.SetNode(node); //gắn vào hàm spamblock cũng dc
         node.blockCtrl = blockCtrl;
     }
 
