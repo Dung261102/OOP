72cd2bf5419ac40ab008bd0eccc294effbd475cb
Dung261102 <20521211@gm.uit.edu.vn>
2024-11-06 13:48:00 +0700

    Thêm hàm lấy và đưa phần từ từ block

--- Pikachu/Assets/_Data/Grid/BreadthFirstSearch.cs
+++ Pikachu/Assets/_Data/Grid/BreadthFirstSearch.cs
@@ -1,58 +1,103 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using Unity.VisualScripting.Antlr3.Runtime.Misc;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class BreadthFirstSearch : GridAbstract, IPathfinding
 {
     [Header("Breadth First Search")]
 
-    public Queue<BlockCtrl> queue = new Queue<BlockCtrl>();
-    public Dictionary<BlockCtrl, BlockCtrl> cameForm = new Dictionary<BlockCtrl, BlockCtrl>();
+    public List<Node> queue = new List<Node>();
+    public List<Node> path = new List<Node>();
+    public Dictionary<Node, Node> cameForm = new Dictionary<Node, Node>();
 
+    //Hàm FnidPath - tìm đường đi 
     public virtual void FindPath(BlockCtrl startBlock, BlockCtrl targetBlock)
     {
         Debug.Log("FindPath");
-        this.queue.Enqueue(startBlock);
-        this.cameForm[startBlock] = startBlock;
+        Node startNode = startBlock.blockData.node;
+        Node targetNode = targetBlock.blockData.node;
+
+
+        this.Enqueue(startNode);
+        this.cameForm[startNode] = startNode;
 
         while (this.queue.Count > 0)
         {
-            BlockCtrl current = this.queue.Dequeue();  
-       
 
-            if (current == targetBlock)
+            Node current = this.Dequeue();
+
+            if (current == targetNode)
             {
+                ConstructPath(startNode, targetNode);
                 break;
             }
 
-            foreach (BlockCtrl neighbor in current.neighbors)
+            foreach (Node neighbor in current.Neighbors())
             {
-                 if (this.IsValidPosition(neighbor) && !cameForm.ContainsKey(neighbor))
+                if (neighbor == null) continue;
+
+
+                if (this.IsValidPosition(neighbor) && !cameForm.ContainsKey(neighbor))
                 {
-                    this.queue.Enqueue(neighbor);
+                    this.Enqueue(neighbor);
                     this.cameForm[neighbor] = current;
                 }
                 
             }
         }
-        this.ShowCameForm();
+        this.ShowPath();
+        
     }
 
-    protected virtual void ShowCameForm ()
+    //ham ConstructPath - tìm đường đi ngắn nghất
+    protected virtual void ConstructPath(Node startNode, Node targetNode)
     {
-        foreach (var pair in cameForm)
+        Node currenCell = targetNode;
+        while (currenCell != startNode)
         {
-            BlockCtrl key = pair.Key;
-            BlockCtrl value = pair.Value;
+            path.Add(currenCell);
+            currenCell = this.cameForm[currenCell];
+        }
 
-            Debug.Log("Left: " + key.ToString() + ", Right: " + value.ToString());
+        path.Add(startNode);
+        path.Reverse();
+    }
+
+    protected virtual void ShowPath()
+    {
+        Vector3 pos;
+        foreach (Node node in this.path)
+        {
+            pos = node.nodeTranform.transform.position;
+            UnityEngine.Transform linker = this.ctrl.blockSpawner.Spawn(BlockSpawner.LINKER, pos, Quaternion.identity);
+            linker.gameObject.SetActive(true);
         }
     }
-    private bool IsValidPosition(BlockCtrl block)
+
+
+    //Hàm enqueue - đưa 1 block vào trong queue
+    protected virtual void Enqueue(Node blockCtrl)
     {
-        return true;
+        this.queue.Add(blockCtrl);
+    }
+
+    //Hàm dequeue - lấy phần tử đầu tiên và cắt nó khỏi list
+    protected virtual Node Dequeue()
+    {
+        Node node = this.queue[0];
+        this.queue.RemoveAt(0);
+        return node;
+    }
+
+   
+    private bool IsValidPosition(Node node)
+    {
+        return !node.occupied; 
+      
     }
 
 }
